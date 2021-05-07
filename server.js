8ef8ab0beac1a4777da24756707a6f6859287b99
Abigail Adams <abigail-adams@gmail.com>
2021-05-07 01:05:32 -0700

    adding functions in server

--- server.js
+++ server.js
@@ -3,33 +3,35 @@ const { prompt } = require("inquirer");
 const db = require("./db/database");
 require("console.table");
 
-function mainMenu() {
-     inquirer.prompt([
+const mainMenu = () => {
+  return inquirer.prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
+      type: "list",
+      message:
+        "Welcome to EmployeeManager, please select from the following menu:",
+      name: "choices",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee",
+      ],
     },
-    {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'Slack', 'smoke signal']
-    },
-    {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'SQL']
-    }
-  // shows user different options they can choose from
-  // i.e. inquirer code
-}
+  ]);
+  inquirer.then((answers) => console.log(answers));
+};
+
+// shows user different options they can choose from
+// i.e. inquirer code
 
 function viewDepartments() {
   // write js to interact with database
   // around 10 lines of code
-  db.findAllDepartments().then(([rows]) => {
+  db.findAllDepartments()
+    .then(([rows]) => {
       let departments = rows;
       console.log("\n");
       console.table(departments);
@@ -37,20 +39,74 @@ function viewDepartments() {
     .then(() => loadMainPrompts());
 }
 
-function viewRoles() {}
+function viewRoles() {
+  db.viewAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      console.log("\n");
+      console.table(roles);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function viewEmployees() {}
+function viewEmployees() {
+  db.viewAllEmployees()
+    .then(([rows]) => {
+      let employee = rows;
+      console.log("\n");
+      console.table(employee);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function addDepartment() {}
+function addDepartment() {
+  db.addAllDepartments()
+    .then(([rows]) => {
+      let departments = rows;
+      console.log("\n");
+      console.table(departments);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function addRole() {}
+function addRole() {
+  db.addAllRoles()
+    .then(([rows]) => {
+      let roles = rows;
+      console.log("\n");
+      console.table(roles);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function addEmployee() {}
+function addEmployee() {
+  db.addAllEmployees()
+    .then(([rows]) => {
+      let employee = rows;
+      console.log("\n");
+      console.table(employee);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function updateEmployeeRole() {}
+function updateEmployeeRole() {
+  db.updateEmployeeRoles()
+    .then(([rows]) => {
+      let employee.roles = rows;
+      console.log("\n");
+      console.table(departments);
+    })
+    .then(() => loadMainPrompts());
+}
 
-function quit() {}
+function quit() {
+  db.quit()
+    .then(([rows]) => {
+      let departments = rows;
+      console.log("\n");
+      console.table(departments);
+    })
+    .then(() => loadMainPrompts());
+}
 
 mainMenu();
-
-
