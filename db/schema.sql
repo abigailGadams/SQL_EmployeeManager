7f378c7f0f1e711d55381d9194adf936867a28f5
Abigail Adams <abigail-adams@gmail.com>
2021-05-06 20:49:24 -0700

    added schema.sql data

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,23 @@ DROP DATABASE IF EXISTS employeeManager_db;
     CREATE DATABASE employeeManager_db;
     USE employeeManager_db;
 
-
-
-CREATE TABLE departments(
+CREATE TABLE Department(
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
 );
 
-CREATE TABLE roles(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  firstName VARCHAR(100),
-  lastName VARCHAR(100),
-  PRIMARY KEY (id)
+CREATE TABLE Roles(
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(5,2),
+  department_id INT 
 );
 
-CREATE TABLE employee(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  firstName VARCHAR(100),
-  lastName VARCHAR(100),
-  PRIMARY KEY (id)
+CREATE TABLE Employee(
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT 
 );
