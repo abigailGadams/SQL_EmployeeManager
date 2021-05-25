708d13d99670849f9dae2a090e7a72e712e3ec41
Abigail Adams <abigail-adams@gmail.com>
2021-05-25 04:08:03 -0700

    updating schema so that no salary cap exists

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,25 @@
 DROP DATABASE IF EXISTS employeeManager_db;
-    CREATE DATABASE employeeManager_db;
+CREATE DATABASE employeeManager_db;
     USE employeeManager_db;
 
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
   id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(5,2),
-  department_id INT,
+  salary DECIMAL(6,0) UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  roles_id INT,
+  roles_id INT UNSIGNED,
   CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE CASCADE,
   manager_id INT,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
